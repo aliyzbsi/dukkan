059309b6f7a37c4c421220f21b6dc0bbc68762ae
aliyzbsi <aliyzbsi68@hotmail.com>
2024-11-21 23:07:39 +0300

    deneme

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={HomePage} />
         </Switch>
-        <div>ali</div>
+        <div>alis</div>
         <div>yüzbaşı</div>
       </main>
     </div>
